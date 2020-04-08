5470dfe54dba9a7a272c6a0352082ee95eba15c4
abelthf <abel.huanca@upeu.edu.pe>
2020-04-08 04:37:42 -0500

    add key

---
+++ keys.py
@@ -0,0 +1,6 @@
+consumer_key = 'wUdvZpBtp5kvNIu8Dyj4JuUDG'
+consumer_secret = 'EZjrY1acy1BMzdkUoZ08vIE3EpeadJtj1ymnxYWD1QScpvDl0Z'
+access_token = '1124495767534596096-GyVJ9iBpIl68RgmU0NODaV2D2aJVw'
+access_token_secret = 'h6POTsZPCeW7UDWzhnm7gxnbOQFZCjrONKQS8H6onCRkD'
+
+mapquest_key = 'YourAPIKey'
